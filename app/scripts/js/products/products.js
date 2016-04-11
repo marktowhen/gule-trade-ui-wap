04f43183e501785d0a016ecc4d7dec56da22d16a
shenmou101 <516211584@qq.com>
2016-04-11 14:28:48 +0800

    修改购物车和所有商品页样式

--- app/scripts/js/products/products.js
+++ app/scripts/js/products/products.js
@@ -1,14 +1,16 @@
 'use strict';
-$(function(){
+function allproducts_onload(){
     var windowheight=$(window).height();
     $(window).scroll(function(){
         if($(window).scrollTop()>=$(document).height()-$(window).height()-70){
-            $('.products ul').append("<li><div class='product_pic'><img src='../images/index/product1.png' alt=''></div><div class='product_name'><b>拉卡拉收款宝 手机pos机移动刷卡</b></div><div class='price_info'><div class='price fl'>￥199.00</div><div class='sales fl'>已售出<span>244</span>笔</div></div></li><li><div class='product_pic'><img src='../images/index/product1.png' alt=''></div><div class='product_name'><b>拉卡拉收款宝 手机pos机移动刷卡</b></div><div class='price_info'><div class='price fl'>￥199.00</div><div class='sales fl'>已售出<span>244</span>笔</div></div></li>")
+            $('.products_products ul').append("<li><div class='product_pic'><img src='../images/index/product1.png' alt=''></div><div class='product_name'><b>拉卡拉收款宝 手机pos机移动刷卡</b></div><div class='price_info'><div class='price fl'>￥199.00</div><div class='sales fl'>已售出<span>244</span>笔</div></div></li><li><div class='product_pic'><img src='../images/index/product1.png' alt=''></div><div class='product_name'><b>拉卡拉收款宝 手机pos机移动刷卡</b></div><div class='price_info'><div class='price fl'>￥199.00</div><div class='sales fl'>已售出<span>244</span>笔</div></div></li>")
         }
     });
-    $('.sort').click(function(){
-        if($('.sort_content').hasClass('hide')){
+    $('allproducts_wrap .sort').click(function(){
+        if($(this).find('.sort_content').hasClass('hide')){
             $('.sort_content').removeClass('hide')
-        }else{$('.sort_content').addClass('hide')}
+        }else{$(this).find('.sort_content').addClass('hide')}
     })
-})
+
+}
+    
