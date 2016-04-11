8910c9db29601e830ea80d7bdba600938abf8970
shenmou101 <516211584@qq.com>
2016-04-11 13:52:48 +0800

    调整购物车和所有商品页的样式和特效

--- app/scripts/js/index/index.js
+++ app/scripts/js/index/index.js
@@ -1,5 +1,6 @@
 'use strict';
-$(function(){
+function index_onload(){
+    //banner滑动
     var swiper = new Swiper('#index_swiper_container', {
         slidesPerView: 1,
         slidesPerColumn: 1,
@@ -8,11 +9,13 @@ $(function(){
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev'
     });
+    
+    //无限加载热销产品
     var windowheight=$(window).height();
     $(window).scroll(function(){
         if($(window).scrollTop()>=$(document).height()-$(window).height()-70){
-            $('.products ul').append("<li><div class='product_pic'><img src='../../images/index/product1.png' alt=''></div><div class='product_name'><b>拉卡拉收款宝 手机pos机移动刷卡</b></div><div class='price_info'><div class='price fl'>￥199.00</div><div class='sales fl'>已售出<span>244</span>笔</div></div></li><li><div class='product_pic'><img src='../../images/index/product1.png' alt=''></div><div class='product_name'><b>拉卡拉收款宝 手机pos机移动刷卡</b></div><div class='price_info'><div class='price fl'>￥199.00</div><div class='sales fl'>已售出<span>244</span>笔</div></div></li>")
+            $('.index_products ul').append("<li><div class='product_pic'><img src='../../images/index/product1.png' alt=''></div><div class='product_name'><b>拉卡拉收款宝 手机pos机移动刷卡</b></div><div class='price_info'><div class='price fl'>￥199.00</div><div class='sales fl'>已售出<span>244</span>笔</div></div></li><li><div class='product_pic'><img src='../../images/index/product1.png' alt=''></div><div class='product_name'><b>拉卡拉收款宝 手机pos机移动刷卡</b></div><div class='price_info'><div class='price fl'>￥199.00</div><div class='sales fl'>已售出<span>244</span>笔</div></div></li>")
         }
     });
 
-});
+};
--- app/scripts/js/shoppingcart/shoppingcart.js
+++ app/scripts/js/shoppingcart/shoppingcart.js
@@ -1,5 +1,54 @@
 'use strict';
-$(function(){
-    $('.choose').height($('.product_pic').height())
-    $('.product_content').height($('.product_pic').height())
-})
+function shoppingcart_onload(){
+    //手机触摸后显示删除商品
+    $('.choose').height($('.product_pic').height());
+    $('.shoppingcart_products .product_content').height($('.product_pic').height());
+    var start_x=0;
+    var stop_x=0;
+    var move_x=0;
+    $('.shoppingcart_products ul li').on('touchstart',function(e){
+        start_x = e.originalEvent.targetTouches[0].clientX;
+    });
+    $('.shoppingcart_products ul li').on('touchmove',function(e){
+        stop_x = e.originalEvent.touches[0].clientX;
+        move_x=stop_x-start_x;
+        if(move_x<-15){
+            $(this).find('.product_body').velocity({translateX:'-16.666667%'},{duration: 100});         
+        }else if(move_x>15){
+            $(this).find('.product_body').velocity({translateX:'0'},{duration: 100});
+        }
+    });
+    
+    //点击选中或取消商品
+    $('.choose i').click(function(){
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+        }else{
+            $(this).addClass('active');
+        }
+    });
+    
+    //增减商品数量
+    $('i.reduce').click(function(){
+        var num=parseInt($(this).next().text());
+        if(num != 1){
+            num--;
+            $(this).next().text(num)
+        }
+    });
+    $('i.add').click(function(){
+        var num=parseInt($(this).prev().text());
+        num++;
+        $(this).prev().text(num)
+    });
+    
+    //全选
+    $('.check_all i').click(function(){
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+        }else{
+            $(this).addClass('active');
+        }
+    });
+}
+    
