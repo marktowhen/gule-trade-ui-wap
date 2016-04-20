86f1850b6cf95ed1d04bd206f914a65c0d5f5870
Mike Cao <michael-cao@outlook.com>
2016-04-20 11:10:02 +0800

    renaming

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,14 +39,14 @@ var wapApp = angular
         url:"/products.html"
       })
     .state('shoppingcart', {
-        templateUrl: 'views/shoppingcart/shoppingcart.html',
+        templateUrl: 'views/orders/shoppingcart.html',
         controller: '',
         url:"/shoppingcart.html"
       })
-    .state('no-payment', {
-        templateUrl: 'views/no-payment/no-payment.html',
+    .state('order-confirmation', {
+        templateUrl: 'views/orders/confirm.html',
         controller: '',
-        url:"/no-payment.html"
+        url:"/orders/confirmation.html"
       })
      .state('user', {
         templateUrl: 'views/user/user.html',
