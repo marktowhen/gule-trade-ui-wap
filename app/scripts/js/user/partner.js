3cac63067fc45f48fb6cf090ebc2f0feef573588
zhurenling <zhurenling@legu.co>
2016-04-26 11:57:59 +0800

    字体图标 样式修改

--- app/scripts/js/user/partner.js
+++ app/scripts/js/user/partner.js
@@ -1,12 +1,11 @@
 "use strict";
 function partner() {
-	$(".partner4-bd ul li").click(
-		function(){
-			if($(this).hasClass("cur")){
+
+	$(document).on('click','.partner4-bd ul li',function(){
+		if($(this).hasClass("cur")){
 				$(this).removeClass("cur");
-			}else{
-				$(this).addClass("cur").siblings().removeClass("cur");
-			}
+		}else{
+			$(this).addClass("cur").siblings().removeClass("cur");
 		}
-	);
+	});
 }
