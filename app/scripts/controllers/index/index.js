9f82f6684dec95f51dec3486bd7f7f53cdddd290
dxf <duanxiaofeng@jingyunbank.com>
2016-05-03 16:08:46 +0800

    update img

--- app/scripts/controllers/index/index.js
+++ app/scripts/controllers/index/index.js
@@ -8,7 +8,22 @@
  * Controller of the jingyunshopApp
  */
 wapApp.controller('IndexController', 
-    function ($scope, $cookies,$state, ConstantService,$stateParams,IndexService) {
+    function ($scope, $cookies,$state, ConstantService,$stateParams,IndexService,GoodsListService) {
 
+    	 $scope.mid=''; //商家id
+         $scope.tid=''; //类型id
+         $scope.order='0'; //排序
+         $scope.name= ''; //商品名
+         $scope.pagefrom = 0;
+         $scope.pagesize = 4;
+
+
+    	 GoodsListService.allGoodsList($scope.mid,$scope.tid,$scope.order,$scope.name,$scope.pagefrom,$scope.pagesize)
+		      .success(function(data){
+         	if(data.code==200){
+         		$scope.showlist = data.body;
+         		console.log($scope.showlist)
+         	}
+		 });
 
 });
