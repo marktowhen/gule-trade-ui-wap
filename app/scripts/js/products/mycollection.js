63de5e80beb4ff74443c806d5ce668baaeb929f7
shenmou101 <516211584@qq.com>
2016-05-03 09:34:13 +0800

    修改js代码 添加js注释 修改链接

--- app/scripts/js/products/mycollection.js
+++ app/scripts/js/products/mycollection.js
@@ -1,8 +1,10 @@
 function mycollection_onload(){
+    //取消收藏
     $(document).on('click','.mycollection_wrap .deselect',function(){
-        $(this).parents('li').addClass('hide')
+        $(this).parents('li').remove()
     });
+    //移至购物车
     $(document).on('click','.mycollection_wrap .movetocart',function(){
-        $(this).parents('li').addClass('hide')
+        $(this).parents('li').remove()
     })
 }
