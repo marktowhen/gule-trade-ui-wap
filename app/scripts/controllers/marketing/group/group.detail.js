b657c6778ad7c13df5ce2b418ca8f5b7c48510bb
QiuXingSheng <qiuxingsheng@jingyunbank.com>
2016-05-03 16:26:02 +0800

    团购

--- app/scripts/controllers/marketing/group/group.detail.js
+++ app/scripts/controllers/marketing/group/group.detail.js
@@ -8,9 +8,9 @@
  * Controller of the jingyunshopApp
  */
 wapApp.controller('GroupDetailController', 
-		 function ($scope, $state,GroupBuyService, $stateParams,GoodsDetailsService) {
+		 function ($scope, $state,GroupBuyService, $stateParams,GoodsDetailsService,MerchantService) {
 	
-
+		//团购商品
 		GroupBuyService.detail($stateParams.ggid)
 			.success(function(data){
 				if(data.ok){
@@ -20,28 +20,62 @@ wapApp.controller('GroupDetailController',
 			}).error(function(data){
 
 			});
-		
+		//商品本身
 		GoodsDetailsService.detail($stateParams.gid).success(function(data2){
 				$scope.goods = data2.body;
-				
 						
 		});
+		//店铺
 
-		var groupID = $stateParams.group_id ;
+
+		$scope.groupID = $stateParams.group_id ;
 		var isEmpty = function(str){
 			if(str==null || str==undefined || str==''){
 				return true;
 			}
 			return false;
 		}
-		if(!isEmpty(groupID)){
+		if(!isEmpty($scope.groupID)){
 
 			//查询参团数量
-			GroupBuyService.countUser(groupID, 'PAID').success(function(data){
+			GroupBuyService.countUser($scope.groupID, 'PAID').success(function(data){
 				$scope.showGroup = true;
 				$scope.count = data.body;
 			})
 		}
+
+		$scope.join = function(){
+			if ($scope.groupID) {
+				var car  = creatCar($scope.groupGoods, $scope.goods,$scope.groupGoods.currentPrice.price);
+				GroupBuyService.join($scope.groupID,car)
+				.success(function(data){
+					if(data.ok){
+						$state.go('orderconfirm.page');
+					}else{
+						alert(data.message);
+					}
+				})
+			}
+			
+		}
+
+		$scope.start = function(){
+			GroupBuyService.start($scope.groupGoods.id,creatCar($scope.groupGoods, $scope.goods,$scope.groupGoods.currentPrice.price))
+				.success(function(data){
+					if(data.ok){
+						$state.go('orderconfirm.page');
+					}else{
+						alert(data.message);
+					}
+				});
+		}
+
+		var creatCar = function(groupGoods, goods, price){
+	    	var goodsInCar = [{'gid':goods.gid,'skuid':groupGoods.skuid,'gname':goods.name,'mid':goods.mid,'mname':goods.mName,'price':price,'count':1}];
+	    	
+	    	var orderInCar = [{'mid':goods.mid,'mname':goods.mName,'postage':0,'type':'GROUP','goods':goodsInCar}];
+	    	return {'orders':orderInCar};
+    	}
 
 		// $scope.getInfo = function(id){
 		// 	GoodsDetailsService.info(gid).success(function(data){
