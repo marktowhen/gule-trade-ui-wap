12644f71881d099437f20a6073257398a9dc0c45
QiuXingSheng <qiuxingsheng@jingyunbank.com>
2016-05-05 17:50:13 +0800

    竞拍

--- app/scripts/controllers/marketing/auction/auction.list.js
+++ app/scripts/controllers/marketing/auction/auction.list.js
@@ -17,6 +17,9 @@ wapApp.controller('AuctionListController',
 			.success(function(data){
 				if(data.ok){
 					for (var i = 0; i < data.body.length; i++) {
+						if('AUCTIONING'==data.body[i].status){
+							getEndTime(data.body[i]);
+						}
 						$scope.ggoods .push( data.body[i]);
 					}
 					if (data.body.length<size) {
@@ -58,6 +61,32 @@ wapApp.controller('AuctionListController',
 		       });	
 			}
 		}
+
+		var getEndTime = function (auction){
+	        var EndTime = new Date(auction.endTime); //截止时间 前端路上 http://www.51xuediannao.com/qd63/
+	        var NowTime = new Date();
+	        var t =EndTime.getTime() - NowTime.getTime();
+
+	        var d=Math.floor(t/1000/60/60/24);
+	        var h=Math.floor(t/1000/60/60%24);
+	        var m=Math.floor(t/1000/60%60);
+	        var s=Math.floor(t/1000%60);
+
+	        auction.end = {'day':d,'hour':h,'minute':m,'second':s};
+	    }
+
+	    var getStartTime = function (auction){
+	        var startTime = new Date(auction.startTime); //截止时间 前端路上 http://www.51xuediannao.com/qd63/
+	        var NowTime = new Date();
+	        var t = NowTime.getTime() - startTime.getTime();
+
+	        var d=Math.floor(t/1000/60/60/24);
+	        var h=Math.floor(t/1000/60/60%24);
+	        var m=Math.floor(t/1000/60%60);
+	        var s=Math.floor(t/1000%60);
+
+	        auction.start = {'day':d,'hour':h,'minute':m,'second':s};
+	    }
 
 		
       
