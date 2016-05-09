174d5044a77a3f354eac80c334bb1cdda18391b3
guoyuxue <guoyuxue@jingyunbank.com>
2016-05-09 15:18:50 +0800

    修改样式

--- app/scripts/controllers/marketing/flashsale/flash-sale-list.js
+++ app/scripts/controllers/marketing/flashsale/flash-sale-list.js
@@ -28,11 +28,12 @@ wapApp.controller('FlashSaleListController', function ($scope, $cookies,$state,$
 	  		$scope.showtime = new Date(fs.showTime);	
 	  		var oft=Math.round(($scope.endtime-new Date())/1000);
 	  					if(oft<=0&&fs.stock>0){
+	  						fs.classes="one-list-box2-right fr";
 			  		 		fs.BtnValue="立即秒杀";
 			  		 		fs.dao=('00:00:00:00');
 			  		 		return TimePromise;
 			  		 	}else if(oft<=0&&fs.stock==0){
-			  		 		$(".one-list-box2-right fr").addClass('gray');
+			  		 		fs.classes="one-list-box2-right fr gray";
 			  		 		fs.BtnValue="已结束";
 			  		 		fs.dao=('00:00:00:00');
 			  		 		return TimePromise;
@@ -41,8 +42,9 @@ wapApp.controller('FlashSaleListController', function ($scope, $cookies,$state,$
 	  				$scope.endtime=new Date(fs.activityTime);
 	  				$scope.showtime = new Date(fs.showTime);	
 	  					var oft=Math.round(($scope.endtime-new Date())/1000);
-			  		 	if(oft>0||fs.stock>0){
-			  		 		$(".one-list-box2-right fr").addClass('green');
+			  		 	if(oft>0&&fs.stock>0){
+			  		 		fs.classes="one-list-box2-right fr green";
+			  		 		/*$(".one-list-box2-right").addClass('fr green');*/
 			  		 		fs.BtnValue="即将开始";
 			  		 		var ofd=parseInt(oft/3600/24);
 							var ofh=parseInt((oft%(3600*24))/3600);
