6b3d87ef9b9232cf1300a1b3b411c30d494831df
alex <myfacebookhello@163.com>
2016-06-17 17:28:59 +0800

    update

--- app/scripts/controllers/user/address/address.list.js
+++ app/scripts/controllers/user/address/address.list.js
@@ -37,21 +37,7 @@ wapApp.controller('OrderAddressListController',
     };
 
     $scope.select = function(address){
-    	if($stateParams.key=="auction"){
-    		if($scope.transaction && $scope.transaction.orders && $scope.transaction.orders.length > 0){
-    			$scope.transaction.addressid = address.id;
-    			$scope.transaction.city = address.city;
-    			$scope.transaction.address = address.countryName+'-'+address.provinceName+'-'+address.cityName+'-'+address.address;
-    			$scope.transaction.receiver = address.receiver;
-    			$scope.transaction.mobile = address.mobile;
-    			$scope.transaction.zipcode = address.zipcode;
-    			$scope.transaction.countryName = address.countryName;
-    			$scope.transaction.provinceName = address.provinceName;
-    			$scope.transaction.cityName = address.cityName;
-    			$scope.selectAddress(address);
-    			$state.go("auction-signup");
-    		}
-    	}else{
+    
     		if($scope.transaction && $scope.transaction.orders && $scope.transaction.orders.length > 0){
     			$scope.transaction.addressid = address.id;
     			$scope.transaction.city = address.city;
@@ -67,7 +53,7 @@ wapApp.controller('OrderAddressListController',
     			$state.go("orderconfirm.page");
     			
     		}
-    	}
+    	
     };
 
     $scope.deleteaddr = function(address){
